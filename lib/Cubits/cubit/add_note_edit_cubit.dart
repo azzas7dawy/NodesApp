a79651cf87857e569f80c82481e5c92eefb56ac7
azzasadawy <azza20saeed@gmail.com>
2024-07-26 03:01:06 +0300

    add logic of cubit

--- lib/Cubits/cubit/add_note_edit_cubit.dart
+++ lib/Cubits/cubit/add_note_edit_cubit.dart
@@ -1,12 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 // ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
+import 'package:notessapp/Models/cardmodel.dart';
+import 'package:notessapp/widgets/constants/colors.dart';
 
 part 'add_note_edit_state.dart';
 
 class AddNoteEditCubit extends Cubit<AddNoteEditState> {
   AddNoteEditCubit() : super(AddNoteEditInitial());
- 
 
-
+ Future addNote(CardModel note) async {
+    emit(AddNoteEditLodeding());
+    try {
+      var notesBox =await Hive.box<CardModel>(kNotesBox);
+      notesBox.add(note);
+      emit(AddNoteEditSuccess());
+    } catch (e) {
+      emit(AddNoteEditFlauiler(e.toString()));
+    }
+  }
 }
