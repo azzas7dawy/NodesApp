76e4cd6586124abc3c0ec25617fcc3225cf66c71
azzasadawy <azza20saeed@gmail.com>
2024-08-01 03:34:09 +0300

    make edit note responsive

--- lib/views/editNotebody.dart
+++ lib/views/editNotebody.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:notessapp/Models/cardmodel.dart';
-
 import 'package:notessapp/views/edit_note_color_list_view.dart';
-
-
 import '../widgets/cutomTextfield.dart';
 
 // ignore: must_be_immutable
@@ -15,33 +11,44 @@ class EditNoteBodyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 40,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 40),
+                    CustomtextFiled(
+                      hint: 'Title',
+                      //note.title,
+                      onChange: (value) {
+                        title = value;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    CustomtextFiled(
+                      hint: note.content,
+                      onChange: (value) {
+                        content = value;
+                      },
+                      maxLines: 8,
+                    ),
+                    const SizedBox(height: 16),
+                    EditNoteColorList(note: note),
+                  ],
+                ),
+              ),
+            ),
           ),
-          CustomtextFiled(
-            hint: note.title,
-            onChange: (value) {
-              title = value;
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomtextFiled(
-            hint: note.content,
-            onChange: (value) {
-              content = value;
-            },
-            maxLines: 8,
-          ),
-          const SizedBox(height: 16),
-           EditNoteColorList(note:note,
-          ),
-          //  NotesModelSheet(),
-        ],
-      ),
+        );
+      },
     );
   }
 }
---
+++ lib/views/noteItem.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notessapp/Cubits/cubit/notes_cubit_cubit.dart';
+import 'package:notessapp/Models/cardmodel.dart';
+
+import 'editView.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({
+    super.key,
+    required this.bColor,
+    required this.note,
+  });
+  final CardModel note;
+
+  final Color bColor;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EditCardView(note:note),
+            ));
+      },
+      child: Container(
+        height: 150,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Color(note.color),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                note.title,
+                style: const TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Text(
+                note.content,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black.withOpacity(0.4),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {
+                    note.delete();
+                    BlocProvider.of<NotesCubitCubit>(context).fetchNotes();
+                  },
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    // Icons.delete,
+                    color: Colors.black,
+                    size: 26,
+                  )),
+            ),
+            Text(
+              note.date,
+              style: const TextStyle(
+                //fontSize: 15,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
